1ba2532feb04bb24ceabe5e648bc57497e984584
utopianf <maoson0307@gmail.com>
2016-02-23 00:34:50 +0900

    modify irc.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # maobot
 an irc bot for KGB
+
+# TODO
+IRCLogger
+IMAGEDownloader
+VIDEODownloader
--- irc.py
+++ irc.py
@@ -2,10 +2,9 @@
 #-*- coding=utf-8 -*-
 
 import socket, string
-from urllib.request import urlopen
-import sql
 
 class irc():
+    """connection with IREnet(irc.ircnet.ne.jp)"""
     def __init__(self):
         self.server = "irc.ircnet.ne.jp"
         self.port = 6667
@@ -17,19 +16,19 @@ class irc():
     def irc_conn(self):
         self.IRC.connect((self.server, self.port))
 
-    def send_data(self, command):
+    def __send_data(self, command):
         self.IRC.send(command.encode("ISO-2022-JP") + b'\n')
 
     def join(self):
-        self.send_data("JOIN %s" % self.channel)
+        self.__send_data("JOIN %s" % self.channel)
 
-    def login(self, nickname="maobot_test", username="maobot_test", password=None,
-            realname="maobot_test", hostname="IRCnet", servername="IRCnet"):
-        self.send_data("USER %s %s %s %s" % (username, hostname, servername, realname))
-        self.send_data("NICK " + nickname)
+    def login(self, username="maobot_test", password=None, realname="maobot_test", 
+            hostname="IRCnet", servername="IRCnet"):
+        self.__send_data("USER %s %s %s %s" % (username, hostname, servername, realname))
+        self.__send_data("NICK " + self.nickname)
 
-    def send_msg(msg):
-        send_data("PRIVMSG %s %s" % (self.channel, msg))
+    def send_msg(self, msg):
+        self.__send_data("PRIVMSG %s %s" % (self.channel, msg))
 
     def test(self):
         self.irc_conn()
@@ -44,7 +43,7 @@ class irc():
                 print("hai")
                 self.send_data("PONG %s" % msg[1])
             if (len(msg) >= 3 and msg[3] == ":PING"):
-                self.send_data("PRIVMSG %s PONG" % self.channel)
+                self.send_msg("PONG")
             if (len(msg) >= 3 and "ガルパン" in msg[3]):
                 self.send_msg("ガルパンはいいぞ")
 
