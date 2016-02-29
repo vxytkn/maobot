dec995fa201ba36119a381a58bc270b12853c03c
utopianf <maoson0307@gmail.com>
2016-02-29 22:24:27 +0900

    Modify logger

--- src/logger.py
+++ src/logger.py
@@ -32,8 +32,10 @@ while(1):
         spker.l_name = u_info[1:u_info.find("!")]
         spker.n_name = u_info[(u_info.find("!")+2):u_info.find("@")]
         if "[enter]#" in buffer:
+            j_ch = buffer[buffer.find("[enter]")+7:]
+            i.join(j_ch)
             with s:
-                s.insert_channel(buffer[buffer.find("[enter]")+7:])
+                s.insert_channel(j_ch)
         with s:
             s.insert_channel(msg[2])
             s.insert_log(spker.l_name, msg[1], msg[2], i_msg[1:])
